06f06ae05d59d27a1bd83b15998ad8d44c48856a
{fujitanao} <fujitanao.rmt@gmail.com>
2022-12-12 17:10:24 +0900

    Initial Commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 import CountrySelector from '@/components/Country/Country';
 import Stats from '@/components/Stats/Stats';
